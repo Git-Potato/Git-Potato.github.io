aa98ab6e9845c326fc8360651d461f5f8d62d541
Potato <betakun1109@gmail.com>
2025-10-28 15:49:19 +0900

    カラーの変更

--- qrgen/sketch.js
+++ qrgen/sketch.js
@@ -1,6 +1,7 @@
 let qrData;
-let url = 'goog';
+let url = 'google.com';//初期表示のURL
 let input;//入力欄
+let inputcolor;
 
 
 function setup() {
@@ -8,18 +9,18 @@ function setup() {
   frameRate(15);
   qrData = qr.encodeQR(url, 'raw');//QRコードのデータを入れる
   input = select('#url')//入力欄を取得
-
+  inputcolor = select('#url');//入力欄の色を取得
+  inputcolor.style('background-color', '#ff6600ff');//入力欄の背景色を変更
 }
 
 function draw() {
-  background(303
-  );
+  background(25, 24, 230);
   textSize(18);
   text(input.value(), 50, 398)
 
   qrData = qr.encodeQR(input.value(), 'raw')
 
-  fill(10, 10, 10.)//塗りの色
+  fill(170, 100, 10.)//塗りの色
   for (let y = 0; y < qrData.length; y++) {
     let line = qrData[y];//y番目の列
     for (let x = 0; x < line.length; x++) {
