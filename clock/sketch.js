fcba707f73ac2e5d5f1eb75387f7999b90510184
Potato <betakun1109@gmail.com>
2025-12-09 16:14:50 +0900

    時計アプリの改良

--- clock/sketch.js
+++ clock/sketch.js
@@ -10,16 +10,26 @@ let week = [
   '土',
 ];
 async function preload() {
-  getData();
+ 
 }
 
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
+  // loadImage();
+  getData();//天気情報を取得する関数を実行
+}
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  colorMode(RGB);
+  fill(253,179,0);
   textAlign(CENTER);
   let date = new Date(); //Dateオブジェクトを取得
   let year = date.getFullYear();//現在の西暦を表示
@@ -30,20 +40,24 @@ function draw() {
   let m = date.getMinutes();//分
   let s = date.getSeconds();//秒
 
-  textSize(40);
+  textSize(50);
 
   textFont('Futura');
   text(year + '/' + month + '/' + day + '(' + week[dow] + ')', 200, 200);
   text(h + ':' + m + ':' + s, 200, 300);
 
-  text(data.current.temperature_2m + '°C', 200, 400);
+  if (data) {
+  text(data.current.temperature_2m + '°C', 200, 400);//気温表示 
 }
 
-async function getData() {
+
+}
+
+//天気情報を取得する
+function getData() {
   
-  data = await loadJSON( "https://api.open-meteo.com/v1/forecast?latitude=36.5667&longitude=139.8833&hourly=temperature_2m,wind_speed_10m,relative_humidity_2m,precipitation_probability&current=is_day,temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,precipitation&timezone=Asia%2FTokyo&forecast_days=1");
-  console.log(data);
-
-
+  loadJSON( "https://api.open-meteo.com/v1/forecast?latitude=36.5667&longitude=139.8833&hourly=temperature_2m,wind_speed_10m,relative_humidity_2m,precipitation_probability&current=is_day,temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m,precipitation&timezone=Asia%2FTokyo&forecast_days=1", newdata =>{
+    data = newdata;//データ取得完了
+    console.log(data);
+  });
 }
-
